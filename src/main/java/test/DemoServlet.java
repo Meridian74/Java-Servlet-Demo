aee6580f5fca46a85bd57acb88e3b2d5fcffc514
DESKTOP-2TO07RP\laszlo.fazekas <laszlo.fazekas@guidance.hu>
2022-02-03 09:44:24 +0100

    Servlet demo 0.9alfa

---
+++ src/main/java/test/DemoServlet.java
@@ -0,0 +1,49 @@
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        // Set the response message's MIME type
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Allocate a output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+            out.println("<title>Test Servlet</title></head>");
+            out.println("<body>");
+            out.println("<h1>Hello, world!</h1>");  // says Hello
+            // Echo client's request information
+            out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+            out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+            out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+            out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+
+            // Echo current time of server
+            out.println("<p>Local time of server: <strong>" + LocalDateTime.now() + "</strong></p>");
+
+            // end of HTML content
+            out.println("</body>");
+            out.println("</html>");
+
+        } finally {
+            out.close();  // Always close the output writer
+        }
+    }
+}
